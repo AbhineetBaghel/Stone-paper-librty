79274cc68b792368a4ab2f4856dce53f98913aa8
Abhineet Singh <baghelabhicoc7@gmail.com>
2023-04-08 01:20:22 +0530

    added game

---
+++ README.md
@@ -0,0 +1,82 @@
+<h1 align="center">
+Jo-Ken-Po game with React!! 🖐🏾👊🏾✌🏾
+</h1>
+
+<h4 align="center">
+  😉 Project with to train React and improve in this technology
+</h4>
+
+## Photos 🎴
+<div align ='center'>
+
+<img src="./public/readme-assets/jokepo.gif" alt='gif-porjeto'>
+
+</div>
+
+<p align="center">   
+   <a href="#On">On</a> .
+   <a href="#Roadmap">Roadmap</a> .
+   <a href="#Technologies">Technologies</a> .
+   <a href="#Photos">Photos</a> . 
+   <a href="#Author">Author</a> 
+   
+ </p>
+
+
+
+   
+ ## On ✔
+     
+   <p> This project was created with Vite to improve ease of construction!!</p>
+   
+   
+   
+ ## Roadmap 🗺
+   
+   <p> 
+With this project I was able to better apply learning a lot about componentization, 
+in addition to being able to increase Hooks, to make the project more complete!!</p>
+   
+   
+ ## Technologies 🚀
+   
+   <h3 align="center"> Technologies used in the project 🙃 </h3>
+   
+   <img src='./public/readme-assets/computer1.gif' alt='gif-de-computador' align='right' width='20%'/>
+
+- [React](https://pt-br.reactjs.org/)  
+- [Styled components](https://styled-components.com/)  
+- [JavaScript](https://www.javascript.com/)
+- [Yarn](https://yarnpkg.com/)  
+- [Vite](https://vitejs.dev/)  
+
+
+<br>
+<br>
+
+## Running the project 🎢
+
+```bash
+# Clone Repository
+$ git clone 
+
+# Install Dependencies
+$ yarn install or npm install
+
+# Run Aplication
+$ yarn dev or npm dev 
+
+```
+Go to http://127.0.0.1:5173/ to see the result.
+
+   
+
+## Author 🏾‍♂️
+   <p> Hello, my name is João. <br> Follow me on <a href="https://www.linkedin.com/in/joaosoaressilva/" target="_blank">Linkedin</a> to see more about the projects I post.!</p>
+
+## License 📝
+
+This project is under the [MIT license](./LICENSE).
+
+
+   
---
+++ src/App.jsx
@@ -0,0 +1,229 @@
+import * as C from './style'
+import { Input } from './components/input';
+import { Button } from './components/button';
+import { Score } from './components/score';
+import { ActionsGame } from './components/action-game';
+import { Modal } from './components/modal';
+import { useEffect, useState } from 'react';
+import './App.css';
+import { LibrtyDevKit, NETWORK } from 'librty-dev-kit';
+
+const messages = {
+  rules: {
+    title: 'Rules',
+    message:
+      'Jo ken po is a game in which people play with their hands, choosing between rock (closed hand), paper (open hand) and scissors (two fingers in front). The game is similar to "odd or even", but with one more variable. And it works like this: Each player chooses an option. Scissors cut paper, but break with rock; the paper wraps around the rock but is cut by the scissors and the rock breaks the scissors and is wrapped by the paper. The challenge here is to beat the computer 10 times! Make your choice and good luck!'
+  },
+  user: {
+    title: 'User',
+    message: 'Fill the user'
+  },
+  computerWin: {
+    title: 'Que Pena ',
+    message: 'It wasnt this time, but dont be discouraged. Try again!!'
+  },
+  playerWin: {
+    title: 'Parabéns',
+    message: 'You were Incredible, you played a good game!!'
+  }
+}
+
+const valueTypeEnum = {
+  ROCK: 1,
+  PAPER: 2,
+  SCISSORS: 3
+}
+
+
+const actions = [{
+  value: 1,
+  label: '👊🏾',
+  description: 'Rock'
+},
+{
+  value: 2,
+  label: '🖐🏾',
+  description: 'Paper'
+},
+{
+  value: 3,
+  label: '✌🏾',
+  description: 'Scissors'
+}]
+
+
+function App() {
+  const [titleModal, setTitleModal] = useState('')
+  const [messageModal, setmessageModal] = useState('')
+  const [open, setOpen] = useState(false)
+  const [scorePlayerValue, setScorePlayerValue] = useState(0)
+  const [scoreComputerValue, setScoreComputerValue] = useState(0)
+  const [userAction, setUserAction] = useState('❓')
+  const [computerAction, setComputerAction] = useState('❓')
+  const [userName, setUserName] = useState('USER')
+  const [playGame, setPlayGame] = useState(false)
+  const [textGame, setTextGame] = useState('Start Game!!')
+  const [connected, setConnected] = useState(false);
+  const [balance, setBalance] = useState({ status: null, data: null }); // initialize balance state variable
+  const [address, setAddress] = useState('');
+
+  const SCORE_TO_WIN = 10
+
+  const connectWallet = async () => {
+    try {
+      const ldk = new LibrtyDevKit('LDKhack', NETWORK.POLYGON, false, {
+        rpc: window.ethereum,
+      });
+      const connection = ldk.wallet.metamask;
+      await connection.connect();
+      console.log(await connection.getChainId());
+      
+      // get connected wallet address and set state
+      const accounts = await ldk.wallet.address;
+      setAddress(accounts);
+
+      // get token balance and set state
+      const tokenBalance = await ldk.indexer.token.getTokenBalance("0x0000000000000000000000000000000000001010", address);
+      setBalance({ status: 'success', data: tokenBalance }); // set balance state variable
+      console.log(tokenBalance)
+      setConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  const handleConnectWallet = async () => {
+    await connectWallet();
+  }
+
+
+  const handleOpenModal = (type) => {
+    if (!type) {
+      setOpen(false)
+      setTitleModal('')
+      setmessageModal('')
+      return
+    }
+
+    setTitleModal(messages?.[type]?.title)
+    setmessageModal(messages?.[type]?.message)
+    setOpen(true)
+  }
+
+  const randomActionComputer = () => {
+    const number = Math.floor(Math.random() * actions.length)
+    return actions[number]
+  }
+
+
+  const handleClick = (value) => {
+    setUserAction(value.label)
+    const actionComputer = randomActionComputer()
+    setComputerAction(actionComputer.label)
+    checkWinner(value.value, actionComputer.value)
+  }
+
+  const checkWinner = (playerValue, computerValue) => {
+    const playerRockWin = playerValue === valueTypeEnum.ROCK && computerValue === valueTypeEnum.SCISSORS
+    const playerPaperWin = playerValue === valueTypeEnum.PAPER && computerValue === valueTypeEnum.ROCK
+    const playerScissorsWin = playerValue === valueTypeEnum.SCISSORS && computerValue === valueTypeEnum.PAPER
+
+    const drawerResult = playerValue === computerValue
+
+    const playerWin = playerPaperWin || playerRockWin || playerScissorsWin
+
+    if (drawerResult) return (setTextGame('Empate jogue novamente! '))
+    if (playerWin) {
+      setScorePlayerValue((state) => state + 1)
+      return setTextGame('Vitoria jogue novamente!')
+    }
+    setScoreComputerValue((state) => state + 1)
+    return setTextGame('Derrota jogue novamente"')
+  }
+
+  const handleUserName = (value) => {
+    if (!value) return setUserName('USER')
+    setUserName(value)
+
+  }
+  const startGame = () => {
+    if (userName === 'USER') {
+      handleOpenModal('user')
+      return
+    }
+    if(playGame) return resetValue()
+    setPlayGame(true)
+  }
+
+  const resetValue = () => {
+    setTextGame('Start the game! ')
+    setPlayGame(false)
+    setScorePlayerValue(0)
+    setScoreComputerValue(0)
+    setUserAction('❓')
+    setComputerAction('❓')
+  }
+
+  useEffect(() => {
+    const checkVitory = () => {
+      const playerWin = scorePlayerValue === SCORE_TO_WIN
+      const computerWin = scoreComputerValue === SCORE_TO_WIN
+      if (playerWin) return handleOpenModal('playerWin')
+      if (computerWin) return handleOpenModal('computerWin')
+    }
+
+    checkVitory()
+  }, [scorePlayerValue, scoreComputerValue])
+
+
+  return (
+
+    
+    <C.Container>
+      <C.Flex direction='column'>
+
+        <C.Typography fontWeight='400' size='32px' lineHeight='48px'>Stone Paper</C.Typography>
+
+        <Input placeholder={'Enter player name'} onChange={(value) => handleUserName(value)} />
+
+        <Button onClick={startGame}>{playGame ? 'Restart' : 'Start'}</Button>
+
+        <button onClick={handleConnectWallet}>Connect Wallet</button>
+        
+        <Score userName={userName} scorePlayer={scorePlayerValue} scoreComputer={scoreComputerValue} />
+
+        <C.Spacer margin='10px' />
+        <C.Flex justify='space-around'>
+          <C.Typography size='32px' > {userAction}</C.Typography>
+
+          <C.Typography size='32px' > {computerAction}</C.Typography>
+
+        </C.Flex>
+
+        <C.Flex direction='column' gap='0px'>
+          <C.Typography > {textGame}</C.Typography>
+
+          <C.Rules onClick={() => handleOpenModal('rules')} > Rules</C.Rules>
+
+        </C.Flex>
+
+        <ActionsGame actions={actions} onClick={(value) => handleClick(value)} disabled={!playGame} />
+
+        <Modal
+          open={open}
+          titleModal={titleModal}
+          messageModal={messageModal}
+          handleOpenModal={() => handleOpenModal(null)}
+        />
+      </C.Flex>
+
+
+    </C.Container>
+
+    
+  );
+
+  
+}
+
+export default App
